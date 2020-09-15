865fdb34a59268f6be90d8d58fccec77e6e76983
bernardkung <bpkung@gmail.com>
2020-09-15 15:14:52 -0400

    Consolidate game state variables to single object

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const app           = express();
 
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
+// app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
 })
 
 // Start App
-app.listen(process.env.PORT, process.env.IP, () => {
-    console.log("The server has started!");
+app.listen(process.env.PORT || 3000, process.env.IP, () => {
+
+    console.log("The server has started: " + toString(process.env.IP) + ":" + process.env.PORT);
 })
 
